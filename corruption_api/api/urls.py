4653b05e8ba1f97bb86c3048b7d56770cdd39806
Costin Bleotu <costi@databus.pro>
2017-10-07 12:55:37 +0300

    rm

--- corruption_api/api/urls.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import, unicode_literals
-from django.conf.urls import include, url
-from django.contrib import admin
-from api import views
-
-
-urlpatterns = [
-    url( r'^winners/$', views.winners_list ),
-]
